f59ce6eeb0b766e2e1795210cc5ff11d8c024c6f
BuiKhanh160905 <khanhbq.23itb@vku.udn.vn>
2024-12-22 01:23:32 +0700

    withRouter

--- src/components/CustomScrollbars.js
+++ src/components/CustomScrollbars.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
-
+import { withRouter } from 'react-router-dom';
 import './CustomScrollbars.scss';
 
 class CustomScrollbars extends Component {
-
     ref = React.createRef();
 
-    getScrollLeft =()=>{
+    getScrollLeft = () => {
         const scrollbars = this.ref.current;
         return scrollbars.getScrollLeft();
-    }
-    getScrollTop =()=>{
+    };
+
+    getScrollTop = () => {
         const scrollbars = this.ref.current;
         return scrollbars.getScrollTop();
-    }
+    };
 
     scrollToBottom = () => {
         if (!this.ref || !this.ref.current) {
@@ -26,65 +26,43 @@ class CustomScrollbars extends Component {
     };
 
     scrollTo = (targetTop) => {
-        const { quickScroll } = this.props;
         if (!this.ref || !this.ref.current) {
             return;
         }
         const scrollbars = this.ref.current;
-        const originalTop = scrollbars.getScrollTop();
-        let iteration = 0;
-
-        const scroll = () => {
-            iteration++;
-            if (iteration > 30) {
-                return;
-            }
-            scrollbars.scrollTop(originalTop + (targetTop - originalTop) / 30 * iteration);
-
-            if (quickScroll && quickScroll === true) {
-                scroll();
-            } else {
-                setTimeout(() => {
-                    scroll();
-                }, 20);
-            }
-        };
-
-        scroll();
+        scrollbars.scrollTop(targetTop); // Cuộn trực tiếp về vị trí mong muốn
     };
 
     renderTrackHorizontal = (props) => {
-        return (
-            <div {...props} className="track-horizontal" />
-        );
+        return <div {...props} className="track-horizontal" />;
     };
 
     renderTrackVertical = (props) => {
-        return (
-            <div {...props} className="track-vertical" />
-        );
+        return <div {...props} className="track-vertical" />;
     };
 
     renderThumbHorizontal = (props) => {
-        return (
-            <div {...props} className="thumb-horizontal" />
-        );
+        return <div {...props} className="thumb-horizontal" />;
     };
 
     renderThumbVertical = (props) => {
-        return (
-            <div {...props} className="thumb-vertical" />
-        );
+        return <div {...props} className="thumb-vertical" />;
     };
 
     renderNone = (props) => {
-        return (
-            <div />
-        );
+        return <div />;
     };
 
+    componentDidUpdate(prevProps) {
+        if (this.props.location.pathname !== prevProps.location.pathname) {
+            if (this.ref.current) {
+                this.scrollTo(0); // Cuộn mượt về đầu trang khi thay đổi route
+            }
+        }
+    }
+
     render() {
-        const { className, disableVerticalScroll, disableHorizontalScroll, children,...otherProps } = this.props;
+        const { className, disableVerticalScroll, disableHorizontalScroll, children, ...otherProps } = this.props;
         return (
             <Scrollbars
                 ref={this.ref}
@@ -104,4 +82,4 @@ class CustomScrollbars extends Component {
     }
 }
 
-export default CustomScrollbars;
+export default withRouter(CustomScrollbars);
